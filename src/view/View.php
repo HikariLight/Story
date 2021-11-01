0a9b4be12f18c36dd4db4e0f6fb8222179295610
HikariLight <merzougui.dhia@gmail.com>
2021-11-01 22:29:45 +0100

    Misc

--- src/view/View.php
+++ src/view/View.php
@@ -73,6 +73,8 @@ require_once("Router.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="../../skin/style.css">
+
     <style>
         <?php echo $this-style; ?>
     </style>
