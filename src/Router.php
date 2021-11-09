c55d5d0caa76b8fa4516b81d7d343d0aac907b55
Julien MERCIER <21802414@etu.unicaen.fr>
2021-11-09 18:01:47 +0100

    Added some Router functionality

--- src/Router.php
+++ src/Router.php
@@ -61,6 +61,13 @@ class Router {
                     $ctl->allUsersPost();
                     break;
 
+                case 'createAccount' : 
+                    $view->makeLoginFormPage();
+                    break;
+
+                case 'login' : 
+                    $view->makeLoginFormPage();
+
                 default : 
                     $view->makeUnknownActionPage();
                     break;
